6b90500621a33da2b32f2cc2e2b8ace195f8fa82
dewijoanne <44321548+dewijoanne@users.noreply.github.com>
2020-10-15 23:05:56 +0700

    Update index.js

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from './components/home';
-import Lobby from './components/lobby';
+import Home from './views/home';
+import Lobby from './views/lobby';
 import Game from './components/game';
 
 Vue.use(VueRouter)
