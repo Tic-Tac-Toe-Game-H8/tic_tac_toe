609476c0bd2623814158f3800f5663dc5dd2e47c
masterambi <ramzyrash@gmail.com>
2020-10-16 05:15:49 +0700

    Add lobby and disconnect endpoint

--- server/app.js
+++ server/app.js
@@ -10,9 +10,29 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+let players = [];
+const board = ['', '', '', '', '', '', '', '', '']
+const status = ''
+
 io.on('connection', (socket) => {
   console.log('a user connected');
+
+  socket.on("playerName", name=> {
+    players.push(name);
+    if (players.length === 2) {
+      io.emit("start", players)
+    }
+  })
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected")
+  })
 });
+
+
+checkWin()
+
+
 
 http.listen(port, () => {
   console.log('listening on PORT 3000');
