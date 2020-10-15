67115fb08ae4f89148948bdc4da7b10734116600
masterambi <ramzyrash@gmail.com>
2020-10-16 06:36:27 +0700

    Update - Rename playerName to userConnect - Rename changeTurn to sendTurn - Change useConnect to send players to connected players - Change setPosition to send position to connected players

--- server/app.js
+++ server/app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const { resetGame, checkForWinCondition } = require("./helpers/tictactoe");
 
 const app = express();
 var http = require("http").createServer(app);
@@ -18,25 +17,37 @@ let turn = 0;
 io.on("connection", (socket) => {
   console.log("a user connected");
 
-  socket.on("playerName", (name) => {
-    players.push(name);
+  socket.on("userConnect", (player) => {
+    players.push(player);
+    io.emit("userConnected", players);
+
+    // Kalau jumlah player udah 2 mulai game dengan mengirim turn (player)
     if (players.length === 2) {
-      io.emit("start", players);
+      resetGame();
+      io.emit("sendTurn", players[turn]);
     }
   });
 
   socket.on("setPosition", (position) => {
     const value = "";
+
+    // Mengirim posisi ke semua player
+    io.emit("sendPosition", position);
+
+    // Untuk mengisi board
     if (turn === 0) value = "x";
     else if (turn === 1) value = "o";
 
     board[position] = value;
+
+    // Check apakah user menang
     if (checkForWinCondition(board, position)) {
-      io.emit("winner", players[turn]);
+      io.emit("sendWinner", players[turn]);
       resetGame();
     } else {
+      // Untuk ganti turn
       turn = turn === 0 ? 1 : 0;
-      io.emit("changeTurn", players[turn]);
+      io.emit("sendTurn", players[turn]);
     }
   });
 
@@ -48,3 +59,27 @@ io.on("connection", (socket) => {
 http.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
+
+const checkForWinCondition = (board, latestPosition) => {
+  const value = board[latestPosition];
+
+  const checkPositions = (a, b, c) =>
+    board[a] === value && board[b] === value && board[c] === value && [a, b, c];
+
+  return (
+    checkPositions(0, 1, 2) ||
+    checkPositions(3, 4, 5) ||
+    checkPositions(6, 7, 8) ||
+    checkPositions(0, 3, 6) ||
+    checkPositions(1, 4, 7) ||
+    checkPositions(2, 5, 8) ||
+    checkPositions(0, 4, 8) ||
+    checkPositions(2, 4, 6)
+  );
+};
+
+const resetGame = () => {
+  turn = 0;
+  players = [];
+  board = ["", "", "", "", "", "", "", "", ""];
+};
